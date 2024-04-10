2978e24c22a360feaa79bea4610ed847151802f4
Rubén Chiquin <randreu28@gmail.com>
2024-04-10 11:07:27 +0200

    (core): Adds initial tests

---
+++ src/main.test.ts
@@ -0,0 +1,20 @@
+import { expect, test } from "bun:test";
+import main from "./main";
+
+test("Low complexity tests", () => {
+  expect(main("L")).toBe("0:0:W");
+  expect(main("R")).toBe("0:0:E");
+  expect(main("F")).toBe("0:1:N");
+});
+
+test("Middle complexity tests", () => {
+  expect(main("RFF")).toBe("2:0:E");
+  expect(main("LFF")).toBe("8:0:W");
+  expect(main("LLFF")).toBe("0:8:S");
+  expect(main("FRFFR")).toBe("2:1:S");
+});
+
+test("High complexity tests", () => {
+  //TODO
+});
+
